c217793e61d07bc458cf07c54945e26587d94ab5
AzhamMakesTrash <azhamproductions@proton.me>
2023-12-21 10:02:50 +0100

    First commit, adding the startup bot, and adding the first ping command

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
-require('dotenv').config(); //initializes dotenv
-const Discord = require('discord.js'); //imports discord.js
+require('dotenv/config'); //initializes dotenv
 
-const client = new Discord.Client({intents:[
-    Discord.GatewayIntentBits.Guilds,
-    Discord.GatewayIntentBits.GuildMessages
-]}); //creates new clients
+const path = require('node:path');
+const fs = require('node:fs');
+const { RE_TOKEN } = require('.env');
+const { Client,GatewayIntentBits } = require('discord.js'); //imports discord.js
+const eventHandler = require("")
 
-client.on('ready', () => {
+const client = new Client({
+    intents:[
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent,
+    ],
+}); //creates new clients
+
+module.exports = client; //exports the client
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(`${commandsPath}/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+client.once(Events.ClientReady, client => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// Commands
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
+client.on(Events.InteractionCreate, async interaction => {
+    const command = client.commands.get(interaction.commandName);
+    try {
+        await command.execute(interaction);
+    }
+    catch(error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
 
 //this line must be at the very end
-client.login(process.env.RE_TOKEN); //signs the bot in with token
+//Well no, it doesn't have to be at the very end, but it's best practice to put it at the end
+client.login(RE_TOKEN); //Logs the bot with the token
